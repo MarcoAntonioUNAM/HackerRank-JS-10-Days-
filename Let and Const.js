63e2cdb45384447aeea4aaf75b10a9c4e9ec40e3
Marco Antonio Arreola <87294765+MarcoAntonioUNAM@users.noreply.github.com>
2021-07-24 23:12:52 -0500

    Add files via upload

---
+++ Let and Const.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    let radio = readLine();
+
+    // Print the area of the circle: - Imprime el area del circulo:
+    console.log(Math.PI * radio * radio);
+
+    // Print the perimeter of the circle: - Imprime el perimetro del circulo:
+    console.log(2 * Math.PI * radio);
+
+
+    try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
